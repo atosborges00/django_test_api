9c2da5684034c06003356cbfe55d4112cdaaa37a
atosborges00 <atosborges00@gmail.com>
2022-10-29 18:49:02 -0300

    feat: using rest_framework decorator

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,14 +2,17 @@ import json
 from pyexpat import model
 from statistics import mode
 from django.forms.models import model_to_dict
-from django.http import JsonResponse
+# from django.http import JsonResponse
 from products.models import Product
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
 
     if model_data:
-        data = model_to_dict(model_data, fields=['id', 'title'])
+        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
 
-    return JsonResponse(data)
+    return Response(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -7,5 +7,7 @@ endpoint = "http://localhost:8000/api/"
 
 get_reponse = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello world"})
 
+# print(get_reponse.text)
+# print(get_reponse.headers)
 print(get_reponse.json())
 # print(get_reponse.status_code)
