bedd45f21bbc6b44874aab76847d3215427ec7c3
atosborges00 <atosborges00@gmail.com>
2022-10-29 15:56:41 -0300

    feat: echoing the request with api views

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,18 @@
+import json
 from django.http import JsonResponse
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "Hi there, this is your Django API response!"})
+    print(request.GET)
+    print(request.POST)
+    body = request.body # byte string JSON data
+    data = {}
+
+    try:
+        data = json.loads(body) # string of JSON -> Python Dict
+    except:
+        pass
+
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -5,7 +5,7 @@ import requests
 # endpoint = 'https://httpbin.org/anything'
 endpoint = "http://localhost:8000/api/"
 
-get_reponse = requests.get(endpoint)
+get_reponse = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello world"})
 
-print(get_reponse.text)
+print(get_reponse.json())
 # print(get_reponse.status_code)
